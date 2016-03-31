dbc37f24d70120add58c511f79ac19eac9ef330a
YoussufRadi <y.sameh1995@gmail.com>
2016-03-31 14:03:39 +0200

    intiallizing server.js

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
+var app = require('./app');
+var http = require('http');
+var db = require('./db.js');
 
+db.connect(function(err) {
+	console.log('connected to db');
+	db.seed(null,function() {
+		console.log('seeded db');
+		app.listen(3000, function() {
+			console.log('app listening on port 3000!');
+		})
+	})
+})
