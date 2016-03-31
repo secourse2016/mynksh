68842939e692152f3830734cb39e634a96672680
YoussufRadi <y.sameh1995@gmail.com>
2016-03-31 16:19:44 +0200

    notes added

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 var db = require('./db.js');
+var htmlTemplate = require('angular-template');
+var bootstrap = require('bootstrap');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
