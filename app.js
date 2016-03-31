0012865b6161956bf6643e60a0cf419e80c6e282
YoussufRadi <y.sameh1995@gmail.com>
2016-03-31 13:59:02 +0200

    intiallizing db in app.js

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
+var express = require('express');
+var app = express();
+var db = require('./db.js');
 
+app.use(express.static(path.join(__dirname, 'public')));
--- db.js
+++ db.js
@@ -2,27 +2,32 @@ var mongo = require('mongodb').MongoClient;
 var DB = null;
 var dbURL = 'mongodb://localhost:27017/quotedb';
 
-    exports.seed = function(cb) {
+
+    //will remove every thing in the database then  seed post to the database
+    exports.seed = function(post,cb) {
         DB.clear(function(err) {
             assert.equal(null, err);
             DB.db().collection('post').insert(post, function(err, result) {
                 assert.equal(null, err);
-                assert.equal(1, result.result.n);
+                // assert.equal(1, result.result.n);
                 cb(err);
             });
         });
-    },
+    };
+
     var connect = exports.connect = function(cb) {
         mongodb.connect(dbUrl, function(err, db) {
             assert.equal(null, err);
             DB = db;
             cb(err, db);
         });
-    },
+    };
+
     exports.db = function() {
         assert.notEqual(null, DB);
         return DB;
-    },
+    };
+
     exports.clearDB = function(done) {
         DB.listCollections().toArray().then(function(collections) {
             collections.forEach(function(c) {
@@ -30,4 +35,4 @@ var dbURL = 'mongodb://localhost:27017/quotedb';
             });
             done();
         }).catch(done);
-    }
+    };
