a811056c02e72c9b2d108b8153af3a2426dddcb3
YoussufRadi <y.sameh1995@gmail.com>
2016-03-31 20:13:22 +0200

    core.js intiallized

--- public/core.js
+++ public/core.js
@@ -1,1 +1,21 @@
+/* Create Angular App Instance */
+App = angular.module('iberia', ['ui.bootstrap', 'ngRoute']);
 
+/**
+ * Angular Routes
+ */
+App.config(function($routeProvider) {
+    $routeProvider
+
+        // route for the home page
+        .when('/', {
+            templateUrl : '/partials/index.html',
+            controller  : 'mainCtrl'
+        })
+
+        // route for the flights page
+        .when('/flights', {
+            templateUrl : '/partials/landingPage.html',
+            controller  : 'flightsCtrl'
+        });
+});
