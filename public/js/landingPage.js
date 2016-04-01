4474aa8573c350d8cbb82fe6539adb227559c649
YoussufRadi <y.sameh1995@gmail.com>
2016-04-01 09:43:09 +0200

    controller name change

--- public/js/landingPage.js
+++ public/js/landingPage.js
@@ -1,9 +1,9 @@
-$(function(){
 
-$('.carousel').carousel({
-    interval: 300
-  });
-$('.carousel-control.right').trigger('click');
+$(function(){
+    $('.carousel').carousel({
+        interval: 300
+      });
+    $('.carousel-control.right').trigger('click');
 });
 
 $(function() {
@@ -14,6 +14,7 @@ $(function() {
         // the name of the box is retrieved using the .attr() method
         // as it is assumed and expected to be immutable
         var group = "input:checkbox[name='" + $box.attr("name") + "']";
+        console.log(group);
         // the checked state of the group/box on the other hand will change
         // and the current value is retrieved using .prop() method
         $(group).prop("checked", false);
@@ -25,7 +26,6 @@ $(function() {
   });
 
  $(function() {
-
 // var landingPage=angular.module('landingPage',[]);
 
 app.controller("PanelController", function(){
