8776a37031afaacf8155f33542a5bcd3499644a3
YoussufRadi <y.sameh1995@gmail.com>
2016-04-01 20:31:50 +0200

    404 error fixed

--- public/controllers/flightsCtrl.js
+++ public/controllers/flightsCtrl.js
@@ -3,6 +3,25 @@
  */
 App.controller('flightsCtrl', function($scope, FlightsSrv) {
 
+	 // $(function() {
+// var landingPage=angular.module('landingPage',[]);
+
+// app.controller("PanelController", function(){
+  
+//   this.tab=1;
+
+//   this.selectTab =function(setTab){
+//     this.tab=setTab;
+//   };
+
+//   this.isSelected = function(checkTab){
+//     return this.tab === checkTab;
+//   };
+
+//  });
+// });
+
+
   /* Retrieve Selected Airports Codes */
   $scope.flight = {
     origin      : FlightsSrv.getSelectedOriginAirport(),
