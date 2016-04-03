8bfea4aa81194ed7c8fc99779d056b1705db6d1b
xXGenomXx <mostafa.ebrahim@hotmail.com>
2016-04-03 20:15:15 +0200

    done with payment srv

--- public/services/paymentSrv.js
+++ public/services/paymentSrv.js
@@ -1,13 +1,59 @@
 /**
  * payment Service
  */
- App.factory('PaymentSrv', function ($http) {
+ App.factory('paymentSrv', function ($http) {
       return {
- 				getOutgoingInfo : function() {
-            			return $http.get('/api/outgoingInfo');
-          	},
-          		getReturnInfo : function() {
-            			return $http.get('/api/returnInfo');
-             }
+        getCountry : function() {
+          return $http.get('/api/data/airports');
+        } ,
+        setSelectedCountry: function(value) {
+          this.setSelectedCountry = value;
+        },
+        getSelectedCountry: function(value) {
+          this.getSelectedCountry = value;
+        },
+        getFirstName : function() {
+          return this.FirstName ;
+        },
+        setFirstName: function(value) {
+          this.FirstName = value;
+        },
+        getSelectedSurname: function() {
+          return this.selectedSurname;
+        },
+        setSelectedSurname: function(value) {
+          this.selectedSurname = value;
+        },
+        getSelectedPassengers: function() {
+          return this.SelectedPassengers;
+        },
+        setSelectedPassengers: function(value) {
+          this.SelectedPassengers = value;
+        },
+        getSelectedStreet: function() {
+          return this.selectedStret;
+        },
+        setselectedStret: function(value) {
+          this.selectedStret = value;
+        },
+        getSelectedinformation: function() {
+          return this.Selectedinformation;
+        },
+        setSelectedinformation: function(value) {
+          this.Selectedinformation = value;
+        },
+        getSelectedPostalcode: function() {
+          return this.selectedPostalcode;
+        },
+        setselectedPostalcode: function(value) {
+          this.selectedPostalcode = value;
+        },
+        getSelectedCity: function() {
+          return this.SelectedCity;
+        },
+        setSelectedCity: function(value) {
+          this.SelectedCity = value;
+        }
+
       };
   });
