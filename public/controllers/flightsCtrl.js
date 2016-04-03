4e2bd48bc57d93d0fcdbe122ab88043355598a95
xXGenomXx <mostafa.ebrahim@hotmail.com>
2016-04-03 18:16:50 +0200

    add button to go to payment

--- public/controllers/flightsCtrl.js
+++ public/controllers/flightsCtrl.js
@@ -1,19 +1,24 @@
 /**
  * Flights Controller
  */
-App.controller('flightsCtrl', function($scope, FlightsSrv,OutReturnSrv) {
+App.controller('flightsCtrl', function($scope, FlightsSrv,OutReturnSrv , $location) {
 
   /* Retrieve Selected Airports Codes */
   // $scope.flight = {
   //   origin      : FlightsSrv.getSelectedOriginAirport(),
   //   destination : FlightsSrv.getSelectedDestinationAirport()
   // };
-   
+
    // $http.get("/api/returnInfo").then(function(response){
    //  $scope.returnInfo= response.data.records;
 
    // });
-  
+
+
+       $scope.goCocontinuePay = function() {
+         $location.url('/payment');
+       };
+
    $scope.roundTrip=true;
    function outgoingInfo() {
     OutReturnSrv.getOutgoingInfo().success(function(outgoingInfo) {
@@ -28,12 +33,12 @@ App.controller('flightsCtrl', function($scope, FlightsSrv,OutReturnSrv) {
   };
   $scope.stringToDate=function(date)
   {
-    return new Date(date); 
+    return new Date(date);
   };
   $scope.timediff = function(depart, arr){
   return moment.utc(moment(arr).diff(moment(depart))).format("hh:mm");
 };
- 
+
 
    $scope.origin= FlightsSrv.getSelectedOriginAirport();
    $scope.dest= FlightsSrv.getSelectedDestinationAirport();
