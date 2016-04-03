34d86f4da17e81cdfacf5d79d3cd9c18dbba02b1
xXGenomXx <mostafa.ebrahim@hotmail.com>
2016-04-03 18:21:39 +0200

    delete my serv after many testing w8ing for controller to work

--- public/services/paymentSrv.js
+++ public/services/paymentSrv.js
@@ -1,22 +1,13 @@
 /**
  * payment Service
  */
-App.factory('paymentSrv', function ($http) {
-     return {
-         getAirportCodes : function() {
-           return $http.get('/api/data/codes');
-         },
-         setSelectedOriginAirport: function(value) {
-           this.selectedOriginAirport = value;
-         },
-         getSelectedOriginAirport: function() {
-           return this.selectedOriginAirport;
-         },
-         setSelectedDestinationAirport: function(value) {
-           this.selectedDestinationAirport = value;
-         },
-         getSelectedDestinationAirport: function() {
-           return this.selectedDestinationAirport;
-         }
-     };
- });
+ App.factory('PaymentSrv', function ($http) {
+      return {
+ 				getOutgoingInfo : function() {
+            			return $http.get('/api/outgoingInfo');
+          	},
+          		getReturnInfo : function() {
+            			return $http.get('/api/returnInfo');
+             }
+      };
+  });
