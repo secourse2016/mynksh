a34a321d316e25c52b375775f9ce0e4fbee86321
hazem279 <hazem.elessawy@gmail.com>
2016-04-04 15:15:22 +0200

    Adding in binding

--- public/controllers/confirmCtrl.js
+++ public/controllers/confirmCtrl.js
@@ -19,20 +19,43 @@ App.controller('confirmCtrl', function($scope, FlightsSrv) {
     var fname;
     var lname;
     $scope.origin = FlightsSrv.getSelectedOriginAirport();
-
+    var flightNumber = "2";
     $scope.dest = FlightsSrv.getSelectedDestinationAirport();
     $scope.oDate = FlightsSrv.getSelectedOutDate();
     $scope.rDate = FlightsSrv.getSelectedReturnDate();
     // $scope.price=FlightsSrv.g
     $scope.tickets = FlightsSrv.getSelectedNumberOfTickets();
 
-    var range = [];
-    range.push(0);
-    for (var i = 1; i < FlightsSrv.getSelectedNumberOfTickets(); i++) {
-        range.push(i);
-    }
 
-    $scope.noOfTickets = range;
+    $scope.reservation = [];
+
+    $scope.typedFname = "";
+    $scope.typedLname = "";
+    $scope.typedCountry = "";
+    $scope.typedPassportno = "";
+    $scope.typedIssueDate = "";
+    $scope.typedExpiryDate = "";
+    $scope.typedEmail = "";
+    $scope.typePhoneno = "";
+
+
+      
+
+
+    for (var bookingRef = 1; bookingRef< 5; bookingRef++) {
+        $scope.ticket = "";
+        $scope.ticket.FName = $scope.typedFname;
+        $scope.ticket.LName = $scope.typedLname;
+        $scope.ticket.country = $scope.typedCountry;
+        $scope.ticket.passportNo = $scope.typedPassportno;
+        $scope.ticket.issueDate = $scope.typedIssueDate;
+        $scope.ticket.expiryDate = $scope.typedExpiryDate;
+        $scope.ticket.email = $scope.typedEmail;
+        $scope.ticket.phone = $scope.typePhoneno;
+        $scope.ticket.refNo = bookingRef;
+        $scope.ticket.flight = flightNumber;
+        $scope.reservation.push($scope.ticket);
+    };
 
     $scope.goToPayment = function() {
         $location.url('/flights');
--- public/js/landingPage.js
+++ public/js/landingPage.js
@@ -1,13 +1,13 @@
 //testing animation with setTimeout
 
-function animation(){
-  var element = document.getElementById("ball"),
-      time    = Date.now();
-  console.log(time);
-  element.style.left = (50 + Math.cos(time / 500)*25) + "px";
-  element.style.top  = (50 + Math.sin(time / 500)*25) + "px";
+// function animation(){
+  // var element = document.getElementById("ball"),
+      // time    = Date.now();
+  // console.log(time);
+  // element.style.left = (50 + Math.cos(time / 500)*25) + "px";
+  // element.style.top  = (50 + Math.sin(time / 500)*25) + "px";
 
-  setTimeout(animation, 1000 / 30);
-}
+  // setTimeout(animation, 1000 / 30);
+// }
 
-animation();
+// animation();
