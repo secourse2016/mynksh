06d7f2f43bd01d2fe518a99cde30eee122be8f80
Narihan Ellaithy <narihanellaithy@gmail.com>
2016-04-04 17:31:07 +0200

    styles added

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,12 +1,12 @@
 /* -----------------------------
 Pre Loader
 ----------------------------- */
-$(window).load(function() {
-	'use strict';
-	$('.loading-icon').delay(500).fadeOut();
-	$('#preloader').delay(800).fadeOut('slow');
-});
-
+// $(window).load(function() {
+// 	'use strict';
+// 	$('.loading-icon').delay(500).fadeOut();
+// 	$('#preloader').delay(800).fadeOut('slow');
+// });
+//
 
 /* -----------------------------
 Backgroung slider
@@ -50,19 +50,19 @@ $(document).ready(function() {
 /* -----------------------------
 NiceScroll
 ----------------------------- */
-$(document).ready(function() {
-	'use strict';
-    $("html").niceScroll({
-		cursorcolor: '#E74C3C',
-		cursoropacitymin: '1',
-		cursorborder: '0px',
-		cursorborderradius: '0px',
-		cursorwidth: '5px',
-		cursorminheight: 60,
-		horizrailenabled: false,
-		zindex: 1090
-	});
-  });
+// $(document).ready(function() {
+// 	'use strict';
+//     $("html").niceScroll({
+// 		cursorcolor: '#E74C3C',
+// 		cursoropacitymin: '1',
+// 		cursorborder: '0px',
+// 		cursorborderradius: '0px',
+// 		cursorwidth: '5px',
+// 		cursorminheight: 60,
+// 		horizrailenabled: false,
+// 		zindex: 1090
+// 	});
+//   });
 
 
 
@@ -164,54 +164,6 @@ $('[placeholder]').focus(function() {
 
 
 
-/* -----------------------------
-Screenshot Load
------------------------------ */
-$(document).ready(function() {
-	'use strict';
-	$('.view-project').on('click', function(e) {
-		e.preventDefault();
-
-		var href 			= $(this).attr('href') + ' .portfolio-project',
-			portfolioWrap	= $('.porfolio-container'),
-			contentLoaded 	= $('#portfolio-load'),
-			offset			= $('#section-screenshots').offset().top;
-
-		portfolioWrap.animate({'left':'-120%'},{duration:400,queue:false});
-		portfolioWrap.fadeOut(400);
-		$('html, body').animate({scrollTop: offset},{duration:800,queue:true});
-		setTimeout(function(){ $('#portfolio-loader').fadeIn('fast'); },300);
-
-		setTimeout(function(){
-            contentLoaded.load(href, function(){
-                $('#portfolio-loader').fadeOut('fast');
-                contentLoaded.fadeIn(600).animate({'left':'0'},{duration:800,queue:false});
-                $('.back-button').fadeIn(600);
-            });
-        },400);
-
-
-
-	});
-
-	$('.backToProject').on('click', function(e){
-		e.preventDefault();
-
-		var portfolioWrap	= $('.porfolio-container'),
-			contentLoaded 	= $('#portfolio-load');
-
-		contentLoaded.animate({'left':'105%'},{duration:400,queue:false}).delay(300).fadeOut(400);
-        $(this).parent().fadeOut(400);
-		setTimeout(function(){
-            portfolioWrap.animate({'left':'0'},{duration:400,queue:false});
-            portfolioWrap.fadeIn(600);
-        },500);
-
-	});
-
-});
-
-
 
 /* -----------------------------
 BxSlider
@@ -241,63 +193,4 @@ $(document).ready(function() {
 		var scrollPos = $(window).scrollTop();
 		scrollPos > 220 ? $('.sticky-section').addClass('nav-bg') : $('.sticky-section').removeClass('nav-bg');
 	});
-});
-
-
-/* -----------------------------
-MailCimp Plugin Script
------------------------------ */
-$(document).ready(function() {
-	'use strict';
-	$('#subscription-form').ajaxChimp({
-		callback: mailchimpCallback,
-		url: 'http://localhost:8080/#/' /* Replace it with your custom URL inside '' */
-	});
-
-	function mailchimpCallback(resp) {
-		 if(resp.result === 'success') {
-			$('.subscription-success')
-				.html(resp.msg)
-				.delay(500)
-				.fadeIn(1000);
-
-			$('.subscription-success').fadeOut(8000);
-
-		} else if(resp.result === 'error') {
-			$('.subscription-failed')
-				.html(resp.msg)
-				.delay(500)
-				.fadeIn(1000);
-
-			$('.subscription-failed').fadeOut(5000);
-		}
-		$('#subscription-form .input-email').val('');
-	};
-});
-
-
-
-/* -----------------------------
-Contact form
------------------------------ */
-$(document).ready(function() {
-	'use strict';
-	$('form.contact-form').on('submit', function(e) {
-		$.post('contact/contact.php', $(this).serialize(), function(response) {
-			if ($('.confirmation p').html() != "") {
-				$('.confirmation p').replaceWith('<p><span class="fa fa-check"></span></p>');
-			}
-			$('.confirmation p').append(response).parent('.confirmation').show();
-			$('html, body').animate({
-				scrollTop: $('#section-contact').offset().top
-				},{duration:800,queue:true});
-			$('.form-item').val('');
-			setTimeout(function() {
-				$('.confirmation').hide();
-			}, 8000);
-		});
-		// disable default action
-		e.preventDefault();
-	});
-
 });
