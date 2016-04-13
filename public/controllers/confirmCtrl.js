b1d204c517d5911910fcc4c90e6e49f8a375d9c7
Kefa7y <midomoka94@hotmail.com>
2016-04-13 17:26:21 +0200

    I believe binding is done , doing some minute testing

--- public/controllers/confirmCtrl.js
+++ public/controllers/confirmCtrl.js
@@ -37,13 +37,7 @@ App.controller('confirmCtrl', function($scope, FlightsSrv, OutReturnSrv, Confirm
     $scope.isGreaterThanTickets = function(num) {
         return num < $scope.number;
     };
-
-    $scope.typedFname = "";
-    $scope.typedLname = "";
-    $scope.typedCountry = "";
-    $scope.typedPassportno = "";
-    $scope.typedIssueDate = "";
-    $scope.typedExpiryDate = "";
+    
     $scope.typedEmail = "";
     $scope.typePhoneno = "";
     var flightNumber = "2";
@@ -53,14 +47,8 @@ App.controller('confirmCtrl', function($scope, FlightsSrv, OutReturnSrv, Confirm
 
     for (var bookingRef = 1; bookingRef <= FlightsSrv.getSelectedNumberOfTickets(); bookingRef++) {
         var ticket = {};
-        ticket.FName = $scope.typedFname;
-        ticket.LName = $scope.typedLname;
-        ticket.country = $scope.typedCountry;
-        ticket.passportNo = $scope.typedPassportno;
-        ticket.issueDate = $scope.typedIssueDate;
-        ticket.expiryDate = $scope.typedExpiryDate;
-        ticket.email = $scope.typedEmail;
-        ticket.phone = $scope.typePhoneno;
+        // ticket.email = $scope.typedEmail;
+        // ticket.phone = $scope.typePhoneno;
         ticket.refNo = bookingRef;
         ticket.flight = flightNumber;
         $scope.reservation.push(ticket);
