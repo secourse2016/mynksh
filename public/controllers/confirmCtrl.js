d10f8921cc6133b77cc0db72ff2ead93e59460f3
hazem279 <hazem.elessawy@gmail.com>
2016-04-14 00:10:42 +0200

    confirmctrl functions needs fixing

--- public/controllers/confirmCtrl.js
+++ public/controllers/confirmCtrl.js
@@ -33,14 +33,14 @@ App.controller('confirmCtrl', function($scope, FlightsSrv, OutReturnSrv, Confirm
 
     $scope.goToPayment = function() {
 
-        if($scope.typePhoneno==null){
+        if($scope.typedPhoneno==null){
             $scope.PhoneNoShow=true;
             return ;
         }
         else
             $scope.PhoneNoShow=false;
 
-        setTicketPhoneNo($scope.typePhoneno);
+        setTicketPhoneNo($scope.typedPhoneno);
 
         if($scope.typedEmail==null){
             $scope.EmailShow=true;
@@ -48,38 +48,39 @@ App.controller('confirmCtrl', function($scope, FlightsSrv, OutReturnSrv, Confirm
         }
         else
             $scope.EmailShow=false;
+        
         setTicketEmail($scope.typedEmail);
         
         for (var x = 1; x <= FlightsSrv.getSelectedNumberOfTickets(); x++) {
-        if($scope.reservation[x].FName==null){
+        if($scope.reservation[x-1].FName==null){
             $scope.FNameShow=true;
             return ;
         }
         else
             $scope.FNameShow=false;
 
-        if($scope.reservation[x].LName==null){
+        if($scope.reservation[x-1].LName==null){
             $scope.LNameShow=true;
             return ;
         }
         else
             $scope.LNameShow=false;
 
-        if($scope.reservation[x].issueDate==null){
+        if($scope.reservation[x-1].issueDate==null){
             $scope.IssueDateShow=true;
             return ;
         }
         else
             $scope.IssueDateShow=false;
 
-         if($scope.reservation[x].expiryDate==null){
+         if($scope.reservation[x-1].expiryDate==null){
             $scope.ExpiryDateShow=true;
             return ;
         }
         else
             $scope.ExpiryDateShow=false;
 
-            if($scope.reservation[x].passportNo==null){
+            if($scope.reservation[x-1].passportNo==null){
             $scope.PassportNumberShow=true;
             return ;
         }
