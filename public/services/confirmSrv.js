0863eae3cb08d1e61a54ee71636c348ce88a10a1
Narihan Ellaithy <narihanellaithy@gmail.com>
2016-04-13 23:45:08 +0200

    kefahy's confirmers

--- public/services/confirmSrv.js
+++ public/services/confirmSrv.js
@@ -3,53 +3,23 @@
  */
 App.factory('ConfirmSrv', function($http) {
     return {
-        setFName: function(value) {
-            this.fname = value;
+        setReservation: function(value) {
+            this.reservation = value;
         },
-        getFName: function(value) {
-            return this.fname;
-        },
-        setLName: function(value) {
-            this.lastName = value;
-        },
-        getLName: function(value) {
-            return this.lastName;
-        },
-        setIssueDate: function(value) {
-            this.issueDate = value;
-        },
-        getIssueDate: function(value) {
-            return this.issueDate;
-        },
-        setExpiryDate: function(value) {
-            this.expiryDate = value;
-        },
-        getExpiryDate: function(value) {
-            return this.expiryDate;
+        getReservation: function() {
+            return this.reservation;
         },
         setEmail: function(value) {
             this.email = value;
         },
-        getEmail: function(value) {
+        getEmail: function() {
             return this.email;
         },
         setPhoneNo: function(value) {
             this.phoneNo = value;
         },
-        getPhoneNo: function(value) {
+        getPhoneNo: function() {
             return this.phoneNo;
-        },
-        setPassportNo: function(value) {
-            this.passportNo = value;
-        },
-        gePassportNo: function(value) {
-            return this.passportNo;
-        },
-        setPassportType: function(value) {
-            this.passportNo = value;
-        },
-        gePassportType: function(value) {
-            return this.passportNo;
         }
     };
 });
