8e7513dea7f925cb762b79d5b3193655b0a5d5e4
Safa Ads <safa.mohamed.ads@gmail.com>
2016-04-15 18:20:01 +0200

    done with both roundTrip and one way routes.js missing seatmap

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -21,53 +21,26 @@ module.exports = function(app, mongo) {
 
 //middleware here!!
 
- app.get('/api/flights/search/:origin/:destination/:departingDate/:returningDate', function(req, res) {
+ app.get('/api/flights/search/:origin/:destination/:departingDate/:returningDate/:cabin', function(req, res) {
         db.connect(function(err,db){
         var collection = db.collection('flights');
         collection.find({"origin": req.params.origin,"destination": req.params.destination,
-            "departureTime": req.params.departingDate, "arrivalTime":req.params.returningDate}).toArray(function(err, flights) {
-        console.log(flights);
-        //cb(err,flights);
-        //console.log(flights);
-          });    
+            "departureTime": req.params.departingDate, "arrivalTime":req.params.returningDate
+            ,"SeatMap.Cabin":req.params.cabin}).toArray(function(err, flights) {
+        res.json(flights);
+           });    
       });
 });  
 
 
-//    app.get('/api/flights/search/:origin/:destination/:departingDate/:returningDate/:cabin', function(req, res) {
-//         db.connect(function(err,db){
-//         var collection = db.collection('flights');
-//         collection.find({"destination": req.params.destination,"origin": req.params.origin,
-//           "departureTime": req.params.departingDate, "arrivalTime":req.params.returningDate,
-//           "SeatMap.Cabin":req.params.cabin}).toArray(function(err, flights) {
-
-//         cb(err,flights);
-//         console.log(flights);
-//           });    
-//       });
-// });  
-
-   // app.get('/api/flights/search/:origin/:departingDate/:class', function(req, res) {
-   //      // retrieve params from req.params.{{origin | departingDate | ...}}
-   //      // return this exact format
-
-   //      return 
-   //      {
-   //        outgoingFlights: 
-   //          [{
-   //              "flightNumber"      : "SE2804",
-   //              "aircraftType"      : "Airbus",
-   //              "aircraftModel"     : "A320",
-   //              "departureDateTime" : 1460478300000,
-   //              "arrivalDateTime"   : 1460478300000,
-   //              "origin"            : "JFK",
-   //              "destination"       : "CAI",
-   //              "cost"              : "1567",
-   //              "currency"          : "USD",
-   //              "class"             : "economy",
-   //              "Airline"           : "United"
-   //          }]
-   //      };
-   //  }); 
+   app.get('/api/flights/search/:origin/:departingDate/:cabin', function(req, res) {
+      db.connect(function(err,db){
+        var collection = db.collection('flights');
+        collection.find({"origin": req.params.origin,"departureTime": req.params.departingDate, 
+        "SeatMap.Cabin":req.params.cabin}).toArray(function(err, flights) {
+        res.json(flights);
+           });    
+      });
+    }); 
 
 };
