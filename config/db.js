8175327daf38ff69e127a4e69603be83e8e0f0ba
Safa Ads <safa.mohamed.ads@gmail.com>
2016-04-15 02:04:18 +0200

    some modifications in db.js

--- config/db.js
+++ config/db.js
@@ -1,36 +1,33 @@
 var mongo = require('mongodb').MongoClient;
- var DB = null;
-
+var DB = null;
 var dbURL = 'mongodb://localhost:27017/mynksh';
 var flights = require('../modules/flights.json');
 
     exports.seed = function(cb) { 
-      connect(function(err,DB){  
-    var collection = DB.collection('flights');
-    collection.count({}, function(err, docs) {
-    if (docs > 0){
-    cb(err,false);
-    }
-    else{
-    collection.insert(flights,{w:1}, function(err, result) {});
-    cb(err,true);
-      }
-  });
-     });
- };
+    connect(function(err,DB){  
+        var collection = DB.collection('flights');
+        collection.count({}, function(err, docs) {
+            if (docs > 0){
+                cb(err,false);
+                }
+            else{
+                collection.insert(flights,{w:1}, function(err, result) {});
+                cb(err,true);
+                }
+            });
+            });
+        };
 
 
    
     var connect = exports.connect = function(cb) {
         mongo.connect(dbURL, function(err, db) {
-            //assert.equal(null, err);
             DB = db;
             cb(err, db);
         });
     };
 
     var dB = exports.db = function() {
-        //assert.notEqual(null, DB);
         return DB;
     };
 
