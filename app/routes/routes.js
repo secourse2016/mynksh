59ab0c25a78ad0dceb6f924d3976e3268648357e
YoussufRadi <y.sameh1995@gmail.com>
2016-04-16 03:13:14 +0200

    api flights re-ordered

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,85 +1,50 @@
-var MongoClient = require('mongodb').MongoClient;
-var db = require("../../config/db.js");
-
-
 module.exports = function(app, mongo) {
 
-    /* GET ALL STATES ENDPOINT */
-    app.get('/api/data/airports', function(req, res) {
-        res.json(require('../../modules/airports.json'));
-    });
+/* GET ALL STATES ENDPOINT */
+app.get('/api/data/airports', function(req, res) {
+    res.json(require('../../modules/airports.json'));
+});
 
-    app.post('/api/data/bookings', function(req, res) {
-        // res.json(require('../../modules/bookings.json'));
-        res.send(require('../../modules/bookings.json'));
-    });
+app.post('/api/data/bookings', function(req, res) {
+    // res.json(require('../../modules/bookings.json'));
+    res.send(require('../../modules/bookings.json'));
+});
 
-    /* RENDER MAIN PAGE */
-    app.get('/', function(req, res) {
-        res.sendFile(__dirname + '/public/index.html');
-    });
+/* RENDER MAIN PAGE */
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/public/index.html');
+});
 
 //middleware here!!
 
-//  app.get('/api/flights/search/:origin/:destination/:departingDate/:returningDate/:cabin', function(req, res) {
-//         db.connect(function(err,db){
-//         var collection = db.collection('flights');
-//         collection.find({"origin": req.params.origin,"destination": req.params.destination,
-//             "departureTime": req.params.departingDate, "arrivalTime":req.params.returningDate
-//             ,"SeatMap.Cabin":req.params.cabin}).toArray(function(err, flights) {
-
-//         res.json(flights);
-//            });
-//       });
-// });
-
-   app.get('/api/flights/search/:origin/:departingDate/:cabin', function(req, res) {
-        var cost = 0;
-        var eOrB= req.params.cabin.toLowerCase();
-      db.connect(function(err,db){
-        var collection = db.collection('flights');
-        collection.find({"origin": req.params.origin,"departureTime": req.params.departingDate}).toArray(function(err, flights) {
-        if(eOrB == "economy"){
-                cost = flights[0].eCost;
-            }
-        else
-            {
-                cost=flights[0].bCost;
-            }
-        if ((eOrB == "economy" && flights[0].availableESeats > 0)  || (eOrB == "business" && flights[0].availableBSeats >0)) {
-        outgoingFlights =
-        {
-          "flightNumber"      :   flights[0].flightNumber,
-          "aircraftType"      :   flights[0].aircraftType,
-          "aircraftModel"     :   flights[0].aircraftModel,
-          "departureDateTime" :   flights[0].departureTime,
-          "arrivalDateTime"   :   flights[0].arrivalTime,
-          "cost"              :   cost,
-          "currency"          :   "EUR",
-          "origin"            :   req.params.origin,
-          "destination"       :   flights[0].destination,
-          "class"             :   eOrB,
-          "Airline"           :   "IBERIA"
-        };
-    }
-        else
-           outgoingFlights = {};
-
-       res.send(JSON.stringify({outgoingFlights}, null, 3));
-           });
-      });
+app.get('/api/flights/search/:origin/:destination/:departingDate/:returningDate/:cabin', function(req, res) {
+  req.db.searchFlights(req.params.origin, req.params.destination, req.params.departingDate, req.params.cabin, function(err, outgoingFlight) {
+    req.db.searchFlights(req.params.destination, req.params.origin, req.params.returningDate, req.params.cabin, function(err, returnFlight) {
+      var flights = {};
+      flights.outgoingFlights = outgoingFlight;
+      flights.returnFlights = returnFlight;
+        res.json(flights);
     });
+  });
+});
+
+app.get('/api/flights/search/:origin/:destination/:departingDate/:cabin', function(req, res) {
+    req.db.searchFlights(req.params.origin, req.params.destination, req.params.departingDate, req.params.cabin, function(err, outgoingFlight) {
+      var flights = {};
+      flights.outgoingFlights = outgoingFlight;
+        res.json(flights);
+    });
+});
 
 
-    app.get('/api/returnInfo', function(req, res) {
-        res.json(require('../../modules/returnInfo.json'));
-    });
-    /* SEED DB */
-    app.get('/db/seed', function(req, res) {
-      req.db.seedDB();
-    });
+app.get('/api/returnInfo', function(req, res) {
+    res.json(require('../../modules/returnInfo.json'));
+});
+/* SEED DB */
+app.get('/db/seed', function(req, res) {
+    req.db.seedDB();
+});
 
-    /* DELETE DB */
-    app.get('/db/delete', function(req, res) {
-    });
+/* DELETE DB */
+app.get('/db/delete', function(req, res) {});
 };
--- config/api.js
+++ config/api.js
@@ -19,3 +19,42 @@ exports.seedDB = function(cb) {
 
     });
 };
+
+exports.searchFlights = function(origin, destination, departingDate, cabin, cb) {
+    var cost = 0;
+    var economyOrBusiness = cabin.toLowerCase();
+    mongo.connect(function(err, db) {
+        var collection = db.collection('flights');
+        collection.find({
+            "origin": origin,
+            "destination": destination,
+            "arrivalTime": departingDate
+        }).toArray(function(err, flights) {
+          if(flights.length === 0)
+          return;
+            if (economyOrBusiness == "economy") {
+                cost = flights[0].eCost;
+            } else {
+                cost = flights[0].bCost;
+            }
+            if ((economyOrBusiness == "economy" && flights[0].availableESeats > 0) || (economyOrBusiness == "business" && flights[0].availableBSeats > 0)) {
+                rflights = {
+                    "flightNumber": flights[0].flightNumber,
+                    "aircraftType": flights[0].aircraftType,
+                    "aircraftModel": flights[0].aircraftModel,
+                    "departureDateTime": flights[0].departureTime,
+                    "arrivalDateTime": flights[0].arrivalTime,
+                    "cost": cost,
+                    "currency": "EUR",
+                    "origin": origin,
+                    "destination": flights[0].destination,
+                    "class": economyOrBusiness,
+                    "Airline": "IBERIA"
+                };
+            } else
+                rflights = {};
+            // mongo.close();
+            cb(err,rflights);
+        });
+    });
+}
