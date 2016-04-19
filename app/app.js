87925d69b3f6f2e482bc965a822d8561554137e5
YaraYehia <yaraasy@gmail.com>
2016-04-19 17:07:41 +0300

    changed 'app.all' to 'app.use'

--- app/app.js
+++ app/app.js
@@ -32,7 +32,7 @@ app.use(express.static(path.join(__dirname, '../public')));
 //       req.db = api;
 //       next();
 // });
-app.all('*', function(req, res, next) {
+app.use('*', function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'X-Requested-With');
   next();
