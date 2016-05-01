ac193599f2492b5edfcca5782dee773549051d65
Narihan Ellaithy <narihanellaithy@gmail.com>
2016-05-01 15:56:41 +0200

    validation for flying to and from

--- www/controllers/landingCtrl.js
+++ www/controllers/landingCtrl.js
@@ -18,10 +18,13 @@ App.controller('landingCtrl', function($scope, FlightsSrv, $location) {
      showCabin:true,
      showPinging:false
    };
-   $scope.dto=new Date();
-    $scope.dtr=new Date();
-   $scope.$on('$ionicView.enter', function(e) {
-     });
+  //  $scope.dto=new Date();
+  //   $scope.dtr=new Date();
+
+
+
+  //  $scope.$on('$ionicView.enter', function(e) {
+  //    });
     //  function reload(){
     //         var current = $state.current;
     //         var params = angular.copy($stateParams);
