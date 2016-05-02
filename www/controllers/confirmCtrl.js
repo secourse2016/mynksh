ae10121fd9488120ccde6ab77f6afe9c658dc591
Kefa7y <midomoka94@hotmail.com>
2016-05-02 21:56:07 +0200

    ionic multi-tickets done

--- www/controllers/confirmCtrl.js
+++ www/controllers/confirmCtrl.js
@@ -7,19 +7,19 @@ App.controller('confirmCtrl', function($scope, FlightsSrv, OutReturnSrv, Confirm
 
     $scope.tickets= FlightsSrv.getTickets();
     if( $scope.tickets == undefined)
-      $scope.tickets = 1;
+      $scope.tickets = 2;
     $scope.price = OutReturnSrv.getSelectedPrice();
     $scope.currentDate = new Date();
     $scope.minDate = new Date($scope.currentDate.getFullYear() -100,$scope.currentDate.getMonth(),$scope.currentDate.getDate());
     $scope.maxDate = new Date($scope.currentDate.getFullYear() +5,$scope.currentDate.getMonth(),$scope.currentDate.getDate());
 
     $scope.reservations = [];
-    // 
-    // for(var i=0 ; i<$scope.tickets ; i++){
-    //     $scope.reservations[i] = {};
-    //     $scope.reservations[i].dateOfBirth = new Date();
-    //     $scope.reservations[i].passportExpiryDate = new Date();
-    // };
+
+    for(var i=0 ; i<$scope.tickets ; i++){
+        $scope.reservations[i] = {};
+        $scope.reservations[i].dateOfBirth = new Date();
+        $scope.reservations[i].passportExpiryDate = new Date();
+    };
 
     var setTicketReservations = function(value) {
         ConfirmSrv.setReservations(value);
