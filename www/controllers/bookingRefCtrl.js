9eec30892c725d68364d72ab8dca15628fd6cee7
Narihan Ellaithy <narihanellaithy@gmail.com>
2016-05-03 16:21:01 +0200

    buttons consistency

--- www/controllers/bookingRefCtrl.js
+++ www/controllers/bookingRefCtrl.js
@@ -41,4 +41,21 @@ function DialogController($scope,$http,BookingSrv, $mdDialog) {
   $scope.answer = function(answer) {
     $mdDialog.hide(answer);
   };
-}
+};
+
+App.filter('unique', function() {
+   return function(collection, keyname) {
+      var output = [],
+          keys = [];
+
+      angular.forEach(collection, function(item) {
+          var key = item[keyname];
+          if(keys.indexOf(key) === -1) {
+              keys.push(key);
+              output.push(item);
+          }
+      });
+
+      return output;
+   };
+});
