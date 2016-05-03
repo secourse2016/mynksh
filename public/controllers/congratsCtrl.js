ca728c3b6eed7573a56ba7ea20c4b9eb3b2534f7
YoussufRadi <y.sameh1995@gmail.com>
2016-05-04 00:22:46 +0200

    airline IP retrieved

--- public/controllers/congratsCtrl.js
+++ public/controllers/congratsCtrl.js
@@ -1,39 +1,36 @@
-App.controller('congratsCtrl', function($scope,paymentSrv,OutReturnSrv,$http) {
+App.controller('congratsCtrl', function($scope, paymentSrv, OutReturnSrv, $http) {
 
 
-	$scope.bookingRefNo1 = paymentSrv.getBookingRefNo1();
-	$scope.bookingRefNo2 = paymentSrv.getBookingRefNo2();
-	$scope.airline1 = OutReturnSrv.getSelectedOutFlight().Airline;
-	$scope.airline2 = OutReturnSrv.getSelectedReturnFlight().Airline;
-	$scope.urlAirline1=paymentSrv.getOtherAirlineIP($scope.airline1).success (function (airlineIP){data});
-	$scope.urlAirline2=paymentSrv.getOtherAirlineIP($scope.airline2).success (function (airlineIP){data});
+  $scope.bookingRefNo1 = paymentSrv.getBookingRefNo1();
+  $scope.bookingRefNo2 = paymentSrv.getBookingRefNo2();
+  $scope.airline1 = OutReturnSrv.getSelectedOutFlight().Airline;
 
-	console.log($scope.bookingRefNo1);
-	console.log($scope.bookingRefNo2);
-	console.log($scope.airline1);
-	console.log($scope.airline2);
+  console.log($scope.bookingRefNo1);
+  console.log($scope.bookingRefNo2);
+  console.log($scope.airline1);
+  console.log($scope.airline2);
 
+  paymentSrv.getOtherAirlineIP1($scope.airline1).success(function(airlineIP) {
+    $scope.urlAirline1 = airlineIP;
+  });
+	
+  paymentSrv.getOtherAirlineIP1($scope.airline1).success(function(airlineIP) {
+    $scope.urlAirline2 = airlineIP;
+  });
 
+  //
+  console.log($scope.urlAirline1);
+  console.log($scope.urlAirline2);
+  $scope.copy = function() {
+    var copyTextarea = document.querySelector('.js-copytextarea');
+    copyTextarea.select();
 
-		$scope.getOtherAirlineIP1=function(){
-			return $http.get('/data/singleAirline/' + $scope.airline1).success();
-		};
-		$scope.getOtherAirlineIP2=function(){
-			return $http.get('/data/singleAirline/' + $scope.airline2).success();
-		};
-		//
-		console.log($scope.urlAirline1);
-		console.log($scope.urlAirline2);
-	$scope.copy = function(){
-        var copyTextarea = document.querySelector('.js-copytextarea');
-        copyTextarea.select();
-
-        try {
-            var successful = document.execCommand('copy');
-            var msg = successful ? 'successful' : 'unsuccessful';
-            console.log('Copying text command was ' + msg);
-        } catch (err) {
-            console.log('Oops, unable to copy');
-        }
-    };
+    try {
+      var successful = document.execCommand('copy');
+      var msg = successful ? 'successful' : 'unsuccessful';
+      console.log('Copying text command was ' + msg);
+    } catch (err) {
+      console.log('Oops, unable to copy');
+    }
+  };
 });
