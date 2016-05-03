b93b4bcae9cc573fd58404c3b06c3fa480e70c21
hazem279 <hazem.elessawy@gmail.com>
2016-05-03 23:47:17 +0200

    first time testing seeddb

--- config/test.js
+++ config/test.js
@@ -1,35 +1,37 @@
 var mongo = require("./api.js");
 var db = require("./db.js");
+var request = require('supertest');
+var assert = require('chai').assert;
 
-var insertPassengers = function(i, passengerDetails, cabin, cost, outgoingFlightId, returnFlightId, data, cb) {
-  if (i === passengerDetails.length) {
-    var fb = {
-      refNum: data,
-      errorMessage: null
-    };
-    cb(fb);
-  } else
-  if (returnFlightId === undefined || returnFlightId === null) {
-    mongo.submitPay(passengerDetails[i].firstName, passengerDetails[i].lastName, passengerDetails[i].passportNum,
-      passengerDetails[i].passportExpiryDate, passengerDetails[i].dateOfBirth, passengerDetails[i].nationality,
-      passengerDetails[i].email, cabin, cost, outgoingFlightId, data, "s",
-      function(err, data2) {
-        insertPassengers(i + 1, passengerDetails, cabin, cost, outgoingFlightId, returnFlightId, data2, cb);
-      });
-  } else {
-    mongo.submitPay(passengerDetails[i].firstName, passengerDetails[i].lastName, passengerDetails[i].passportNum,
-      passengerDetails[i].passportExpiryDate, passengerDetails[i].dateOfBirth, passengerDetails[i].nationality,
-      passengerDetails[i].email, cabin, cost, outgoingFlightId, data, "s",
-      function(err, data2) {
-        mongo.submitPay(passengerDetails[i].firstName, passengerDetails[i].lastName, passengerDetails[i].passportNum,
-          passengerDetails[i].passportExpiryDate, passengerDetails[i].dateOfBirth, passengerDetails[i].nationality,
-          passengerDetails[i].email, cabin, cost, returnFlightId, data2, "r",
-          function(err, data3) {
-            insertPassengers(i + 1, passengerDetails, cabin, cost, outgoingFlightId, returnFlightId, data3, cb);
-          });
-      });
-  } //end of else
-}
+// var insertPassengers = function(i, passengerDetails, cabin, cost, outgoingFlightId, returnFlightId, data, cb) {
+//   if (i === passengerDetails.length) {
+//     var fb = {
+//       refNum: data,
+//       errorMessage: null
+//     };
+//     cb(fb);
+//   } else
+//   if (returnFlightId === undefined || returnFlightId === null) {
+//     mongo.submitPay(passengerDetails[i].firstName, passengerDetails[i].lastName, passengerDetails[i].passportNum,
+//       passengerDetails[i].passportExpiryDate, passengerDetails[i].dateOfBirth, passengerDetails[i].nationality,
+//       passengerDetails[i].email, cabin, cost, outgoingFlightId, data, "s",
+//       function(err, data2) {
+//         insertPassengers(i + 1, passengerDetails, cabin, cost, outgoingFlightId, returnFlightId, data2, cb);
+//       });
+//   } else {
+//     mongo.submitPay(passengerDetails[i].firstName, passengerDetails[i].lastName, passengerDetails[i].passportNum,
+//       passengerDetails[i].passportExpiryDate, passengerDetails[i].dateOfBirth, passengerDetails[i].nationality,
+//       passengerDetails[i].email, cabin, cost, outgoingFlightId, data, "s",
+//       function(err, data2) {
+//         mongo.submitPay(passengerDetails[i].firstName, passengerDetails[i].lastName, passengerDetails[i].passportNum,
+//           passengerDetails[i].passportExpiryDate, passengerDetails[i].dateOfBirth, passengerDetails[i].nationality,
+//           passengerDetails[i].email, cabin, cost, returnFlightId, data2, "r",
+//           function(err, data3) {
+//             insertPassengers(i + 1, passengerDetails, cabin, cost, outgoingFlightId, returnFlightId, data3, cb);
+//           });
+//       });
+//   } //end of else
+// }
 
 var newres = [{
   "firstName": "qwwww",
@@ -54,7 +56,7 @@ var newres = [{
   "passportExpiryDate": "zzzzz",
   "dateOfBirth": "zzzzz",
   "nationality": "zzzzz",
-  "email": "zzzzz",
+  "email": "zzzzz", 
 }, {
   "firstName": "vvvvvvvv",
   "lastName": "vvvvvvvv",
@@ -88,4 +90,27 @@ insertPassengers(0, paymentInfo.passengerDetails, paymentInfo.class, paymentInfo
   function(fb) {
     console.log(fb);
   });
+});
+
+
+describe('seedDB test', function() {
+  var model=db.db().collection('airLines').length();
+  it('Should return the no. of elements in the db', function(done){
+    mongo.seedDB();
+    assert.equal(model,db().collection('airLines'));
+    .expect(200,done);
+
+  });
+    // it('/api/quote should return a quote JSON object with keys [_id, text, author]', function(done) {
+    //     request.get('/api/quote').set('Accept','application/json').expect('Content-Type',/json/).expect(function(res){
+    //         assert.property(res.body,'_id','object has an id');
+    //         assert.property(res.body,'text','object has a text');
+    //         assert.property(res.body,'author','object has an author');
+    //         // assert.isString(res.body.author);
+    //     }).expect(200,done);
+    // });
+
+    // it('/api/quotes should return an array of JSON object when I visit', function(done) {
+    //     request.get('/api/quotes').set('Accept','application/json').expect('Content-Type',/json/).expect(200,done);
+    // });
 });
