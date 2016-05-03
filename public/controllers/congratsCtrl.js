82f94519bc1d1beb161f418d52d85ffb22285022
YoussufRadi <y.sameh1995@gmail.com>
2016-05-03 20:33:00 +0200

    airlines added in congrats

--- public/controllers/congratsCtrl.js
+++ public/controllers/congratsCtrl.js
@@ -2,7 +2,8 @@ App.controller('congratsCtrl', function($scope,paymentSrv) {
 
 	$scope.bookingRefNo1 = paymentSrv.getBookingRefNo1();
 	$scope.bookingRefNo2 = paymentSrv.getBookingRefNo2();
-
+	$scope.airline1 = paymentSrv.getAirLine1();
+	$scope.airline2 = paymentSrv.getAirLine2();
 	$scope.copy = function(){
         var copyTextarea = document.querySelector('.js-copytextarea');
         copyTextarea.select();
