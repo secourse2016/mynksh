170245a535f71c9ae9b8d3143d7457f92a0c2b4b
YoussufRadi <y.sameh1995@gmail.com>
2016-05-03 02:13:17 +0200

    Payment ionic configured

--- public/services/paymentSrv.js
+++ public/services/paymentSrv.js
@@ -2,7 +2,6 @@ App.factory('paymentSrv', function($http) {
   return {
 
     getOtherAirlineIP: function(airlineName) {
-      jwt = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJNWU5LU0giLCJpYXQiOjE0NjA3NzIyOTQsImV4cCI6MTQ5MjMwODI5NSwiYXVkIjoid3d3LnNlY291cnNlLmNvbSIsInN1YiI6Ik1ZTktTSCBJYmVyaWEiLCJUZWFtIjoiTVlOS1NIIn0.hZxhv8XAcu1cARgcrtfb0l_crF1-Ic1tJt9eUhIL0qQ';
       return $http.get('/data/singleAirline/' + airlineName)
     },
     getOtherStripePubKey: function(airlineIP) {
--- www/services/paymentSrv.js
+++ www/services/paymentSrv.js
@@ -1,81 +1,51 @@
-
 App.factory('paymentSrv', function($http) {
-    return {
+  return {
 
-        postPayS: function(reservation,  outgoingFlight , cabin) {
-            return $http.get('http://localhost:8080/data/pay/'+ reservation.FName + '/' + reservation.LName + '/' + reservation.country + '/' + reservation.passportNo+'/'+
-            reservation.issueDate+'/'+ reservation.expiryDate + '/' + reservation.email + '/' + reservation.phoneno + '/'+outgoingFlight.flightNumber+'/'+cabin);
-        },
-        postPayR: function(reservation,  outgoingFlight , returnFlight , cabin) {
-            return $http.get('http://localhost:8080/data/pay/'+ reservation.FName + '/' + reservation.LName + '/' + reservation.country + '/' + reservation.passportNo+'/'+
-            reservation.issueDate+'/'+ reservation.expiryDate + '/' + reservation.email + '/' + reservation.phoneno + '/' +outgoingFlight.flightNumber+
-            '/'+returnFlight.flightNumber+'/'+cabin);
-        },
-        // postPay: function(reservation,  outgoingFlight , returnFlight , cabin) {
-        //     return $http.get('http://localhost:8080/data/pay/'+ "safa" + '/' + "radwa" + '/' + "cairo" + '/' + "0105"+'/'+
-        //     2016-01-01+'/'+   2016-0-01 + '/' + "radwa_Ads" + '/' + 01061575730 + '/' +outgoingFlight.flightNumber+
-        //     '/'+returnFlight.flightNumber+'/'+cabin);
-        // },
-        getSelectedCardType: function() {
-            return this.SelectedCardType;
-        },
-        setSelectedCardType: function(value) {
-            this.SelectedCardType = value;
-        },
-        getSelectedCardNo: function() {
-            return this.SelectedCardNo;
-        },
-        setSelectedCardNo: function(value) {
-            this.SelectedCardNo = value;
-        },
-        getSelectedMonth: function() {
-            return this.SelectedMonth;
-        },
-        setSelectedMonth: function(value) {
-            this.SelectedMonth = value;
-        },
-        getSelectedYear: function() {
-            return this.SelectedYear;
-        },
-        setSelectedYear: function(value) {
-            this.SelectedYear = value;
-        },
-        getSelectedCVV: function() {
-            return this.SelectedCVV;
-        },
-        setSelectedCVV: function(value) {
-            this.SelectedCVV = value;
-        },
-        getSelectedStreet: function() {
-            return this.selectedStret;
-        },
-        setSelectedStreet: function(value) {
-            this.selectedStret = value;
-        },
-        getSelectedInformation: function() {
-            return this.Selectedinformation;
-        },
-        setSelectedInformation: function(value) {
-            this.Selectedinformation = value;
-        },
-        getSelectedPostalcode: function() {
-            return this.selectedPostalcode;
-        },
-        setSelectedPostalcode: function(value) {
-            this.selectedPostalcode = value;
-        },
-        getBookingRefNo: function() {
-            return this.bookingRefNo;
-        },
-        setBookingRefNo: function(value) {
-            this.bookingRefNo = value;
-        },
-        getSelectedCity: function() {
-            return this.SelectedCity;
-        },
-        setSelectedCity: function(value) {
-            this.SelectedCity = value;
-        }
-
-    };
+    getOtherAirlineIP: function(airlineName) {
+      return $http.get('http://localhost:8080/data/singleAirline/' + airlineName)
+    },
+    getOtherStripePubKey: function(airlineIP) {
+      jwt = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJNWU5LU0giLCJpYXQiOjE0NjA3NzIyOTQsImV4cCI6MTQ5MjMwODI5NSwiYXVkIjoid3d3LnNlY291cnNlLmNvbSIsInN1YiI6Ik1ZTktTSCBJYmVyaWEiLCJUZWFtIjoiTVlOS1NIIn0.hZxhv8XAcu1cARgcrtfb0l_crF1-Ic1tJt9eUhIL0qQ';
+      if (airlineIP === "IBERIA")
+        return $http.get('http://localhost:8080/stripe/pubkey/?wt=' + jwt);
+      else {
+        console.log("hena");
+        return $http.get('http://' + airlineIP + '/stripe/pubkey/?wt=' + jwt);
+      }
+    },
+    chargeCard: function(paymentInfo, pingIp) {
+      var jwt = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJNWU5LU0giLCJpYXQiOjE0NjA3NzIyOTQsImV4cCI6MTQ5MjMwODI5NSwiYXVkIjoid3d3LnNlY291cnNlLmNvbSIsInN1YiI6Ik1ZTktTSCBJYmVyaWEiLCJUZWFtIjoiTVlOS1NIIn0.hZxhv8XAcu1cARgcrtfb0l_crF1-Ic1tJt9eUhIL0qQ';
+      return $http.post(pingIp + '/booking?wt=' + jwt, paymentInfo);
+    },
+    getSelectedCardNo: function() {
+      return this.SelectedCardNo;
+    },
+    setSelectedCardNo: function(value) {
+      this.SelectedCardNo = value;
+    },
+    getSelectedMonth: function() {
+      return this.SelectedMonth;
+    },
+    setSelectedMonth: function(value) {
+      this.SelectedMonth = value;
+    },
+    getSelectedYear: function() {
+      return this.SelectedYear;
+    },
+    setSelectedYear: function(value) {
+      this.SelectedYear = value;
+    },
+    getSelectedCVV: function() {
+      return this.SelectedCVV;
+    },
+    setSelectedCVV: function(value) {
+      this.SelectedCVV = value;
+    },
+    getBookingRefNo: function() {
+      return this.bookingRefNo;
+    },
+    setBookingRefNo: function(value) {
+      this.bookingRefNo = value;
+    }
+  };
 });
