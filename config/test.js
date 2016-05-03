f9ac9cc2ecec8c837eb9dd5dce5706c6a695766c
hazem279 <hazem.elessawy@gmail.com>
2016-05-04 00:23:10 +0200

    wrote test for seeding and clearing DB from api

--- config/test.js
+++ config/test.js
@@ -3,35 +3,35 @@ var db = require("./db.js");
 var request = require('supertest');
 var assert = require('chai').assert;
 
-// var insertPassengers = function(i, passengerDetails, cabin, cost, outgoingFlightId, returnFlightId, data, cb) {
-//   if (i === passengerDetails.length) {
-//     var fb = {
-//       refNum: data,
-//       errorMessage: null
-//     };
-//     cb(fb);
-//   } else
-//   if (returnFlightId === undefined || returnFlightId === null) {
-//     mongo.submitPay(passengerDetails[i].firstName, passengerDetails[i].lastName, passengerDetails[i].passportNum,
-//       passengerDetails[i].passportExpiryDate, passengerDetails[i].dateOfBirth, passengerDetails[i].nationality,
-//       passengerDetails[i].email, cabin, cost, outgoingFlightId, data, "s",
-//       function(err, data2) {
-//         insertPassengers(i + 1, passengerDetails, cabin, cost, outgoingFlightId, returnFlightId, data2, cb);
-//       });
-//   } else {
-//     mongo.submitPay(passengerDetails[i].firstName, passengerDetails[i].lastName, passengerDetails[i].passportNum,
-//       passengerDetails[i].passportExpiryDate, passengerDetails[i].dateOfBirth, passengerDetails[i].nationality,
-//       passengerDetails[i].email, cabin, cost, outgoingFlightId, data, "s",
-//       function(err, data2) {
-//         mongo.submitPay(passengerDetails[i].firstName, passengerDetails[i].lastName, passengerDetails[i].passportNum,
-//           passengerDetails[i].passportExpiryDate, passengerDetails[i].dateOfBirth, passengerDetails[i].nationality,
-//           passengerDetails[i].email, cabin, cost, returnFlightId, data2, "r",
-//           function(err, data3) {
-//             insertPassengers(i + 1, passengerDetails, cabin, cost, outgoingFlightId, returnFlightId, data3, cb);
-//           });
-//       });
-//   } //end of else
-// }
+var insertPassengers = function(i, passengerDetails, cabin, cost, outgoingFlightId, returnFlightId, data, cb) {
+  if (i === passengerDetails.length) {
+    var fb = {
+      refNum: data,
+      errorMessage: null
+    };
+    cb(fb);
+  } else
+  if (returnFlightId === undefined || returnFlightId === null) {
+    mongo.submitPay(passengerDetails[i].firstName, passengerDetails[i].lastName, passengerDetails[i].passportNum,
+      passengerDetails[i].passportExpiryDate, passengerDetails[i].dateOfBirth, passengerDetails[i].nationality,
+      passengerDetails[i].email, cabin, cost, outgoingFlightId, data, "s",
+      function(err, data2) {
+        insertPassengers(i + 1, passengerDetails, cabin, cost, outgoingFlightId, returnFlightId, data2, cb);
+      });
+  } else {
+    mongo.submitPay(passengerDetails[i].firstName, passengerDetails[i].lastName, passengerDetails[i].passportNum,
+      passengerDetails[i].passportExpiryDate, passengerDetails[i].dateOfBirth, passengerDetails[i].nationality,
+      passengerDetails[i].email, cabin, cost, outgoingFlightId, data, "s",
+      function(err, data2) {
+        mongo.submitPay(passengerDetails[i].firstName, passengerDetails[i].lastName, passengerDetails[i].passportNum,
+          passengerDetails[i].passportExpiryDate, passengerDetails[i].dateOfBirth, passengerDetails[i].nationality,
+          passengerDetails[i].email, cabin, cost, returnFlightId, data2, "r",
+          function(err, data3) {
+            insertPassengers(i + 1, passengerDetails, cabin, cost, outgoingFlightId, returnFlightId, data3, cb);
+          });
+      });
+  } //end of else
+}
 
 var newres = [{
   "firstName": "qwwww",
@@ -94,13 +94,51 @@ insertPassengers(0, paymentInfo.passengerDetails, paymentInfo.class, paymentInfo
 
 
 describe('seedDB test', function() {
-  var model=db.db().collection('airLines').length();
+   before(db.clearDB);
+    it('should populate the db if db is empty returning true', function(done) {
+        mongo.seedDB(function(error, seeded){
+            assert.equal(seeded,true,'DB has seeded the items');
+            done();    
+        });
+    });
+    it('should have populated the airlines collection with 27 document', function(done) {
+        mongo.seedDB(function(error, seeded){
+            db.db().collection('airLines').toArray((function(err,items){
+                assert.lengthOf(items.length,27,'DB has 27 items');
+                done();
+            });    
+        });
+    });
+    it('should not seed db again if db is not empty returning false in the callback', function(done) {
+        mongo.seedDB(function(error, seeded){
+                mongo.seedDB(function(error, seeded){
+                assert.equal(seeded,false,'DB has already seeded the quotes once');
+                done();    
+            });    
+        });
+    });
+    it('should not seed db again if db is not empty', function(done) {
+        mongo.seedDB(function(error, seeded){
+                mongo.seedDB(function(error, seeded){
+                    db.db().collection('airLines').toArray((function(err,items){
+                    assert.lengthOf(items,27,'DB has 27 items');
+                    done();
+                });     
+            });    
+        });
+    });
+});
+
+
+describe('clearDB test', function() {
   it('Should return the no. of elements in the db', function(done){
-    mongo.seedDB();
-    assert.equal(model,db().collection('airLines'));
-    .expect(200,done);
+    mongo.clearDB();
+    assert.equal(0,db().collection('airLines').totalSize());
 
   });
+});
+
+
     // it('/api/quote should return a quote JSON object with keys [_id, text, author]', function(done) {
     //     request.get('/api/quote').set('Accept','application/json').expect('Content-Type',/json/).expect(function(res){
     //         assert.property(res.body,'_id','object has an id');
@@ -113,4 +151,4 @@ describe('seedDB test', function() {
     // it('/api/quotes should return an array of JSON object when I visit', function(done) {
     //     request.get('/api/quotes').set('Accept','application/json').expect('Content-Type',/json/).expect(200,done);
     // });
-});
+// });
