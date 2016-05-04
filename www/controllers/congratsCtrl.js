35a5bc3312081f96de36dd36e0467f4a008e31e7
Narihan Ellaithy <narihanellaithy@gmail.com>
2016-05-04 18:00:52 +0200

    fixing error

--- www/controllers/congratsCtrl.js
+++ www/controllers/congratsCtrl.js
@@ -1,6 +1,6 @@
-App.controller('congratsCtrl', function($scope, $location, paymentSrv, OutReturnSrv) {
+App.controller('congratsCtrl', function($scope, $location, paymentSrv, OutReturnSrv,FlightsSrv) {
   $scope.bookingRefNo1 = paymentSrv.getBookingRefNo1();
-  
+
   $scope.airline1 = OutReturnSrv.getSelectedOutFlight().Airline;
   $scope.roundTrip = FlightsSrv.getSelectedRoundTrip();
   if ($scope.roundTrip === 'true'){
